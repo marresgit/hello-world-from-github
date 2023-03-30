e3a6ded33b9bc3d2c2a4c005cc3761a4bf9aeb27
Mauricio Saez <saeztoro.mauricio@gmail.com>
2023-03-30 22:49:22 +0200

    Adding new python code with a short lambda function? what? xD

---
+++ hello-world.py
@@ -0,0 +1,12 @@
+import requests
+import json
+def handlar(event, context):
+    response = requests.get("https://jsonplaceholder.typicode.com/todos/1")
+    res = {
+        "event": event,
+        "output": response.json(),
+        "context": context
+    }
+    print(res)
+
+    return None
